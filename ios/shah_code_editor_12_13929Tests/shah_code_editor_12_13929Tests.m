5c63c6afb15222b3202d51c374d8fabbe5a5c165
Crowdbotics <team@crowdbotics.com>
2020-10-23 15:43:13 +0000

    Initial commit.

---
+++ ios/shah_code_editor_12_13929Tests/shah_code_editor_12_13929Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface shah_code_editor_12_13929 : XCTestCase
+
+@end
+
+@implementation shah_code_editor_12_13929
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
