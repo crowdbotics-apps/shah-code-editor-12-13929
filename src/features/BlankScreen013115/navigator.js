da67fe40783446151d81ba831bf85b78ed6e34a7
Shah RukhKhan <shah@crowdbotics.com>
2020-10-26 14:06:47 +0500

    Update screen

--- src/features/BlankScreen013115/navigator.js
+++ src/features/BlankScreen013115/navigator.js
@@ -1,7 +1,6 @@
 import { createStackNavigator } from "react-navigation-stack";
 
 import Blank from "./screens/blank";
-
 export default BlankBlueprintNavigator = createStackNavigator(
   {
     Blank: { screen: Blank }
