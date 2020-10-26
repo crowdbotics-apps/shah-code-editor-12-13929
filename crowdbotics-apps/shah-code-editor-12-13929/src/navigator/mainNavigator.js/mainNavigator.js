489fa0ae800354953d6c6c763cfb3289683b1b43
Shah RukhKhan <shah@crowdbotics.com>
2020-10-26 14:43:40 +0500

    Update screen

---
+++ crowdbotics-apps/shah-code-editor-12-13929/src/navigator/mainNavigator.js/mainNavigator.js
@@ -0,0 +1,38 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+
+import SplashScreen from "../features/SplashScreen";
+import SideMenu from './sideMenu';
+//@BlueprintImportInsertion
+import BlankScreen113116Navigator from '../features/BlankScreen113116/navigator';
+import BlankScreen013115Navigator from '../features/BlankScreen013115/navigator';
+
+/**
+ * new navigators can be imported here
+ */
+
+const AppNavigator = {
+
+    //@BlueprintNavigationInsertion
+BlankScreen113116: { screen: BlankScreen113116Navigator },
+BlankScreen013115: { screen: BlankScreen013115Navigator },
+
+    /** new navigators can be added here */
+    SplashScreen: {
+      screen: SplashScreen
+    }
+};
+
+const DrawerAppNavigator = createDrawerNavigator(
+  {
+    ...AppNavigator,
+  },
+  {
+    contentComponent: SideMenu
+  },
+);
+
+const AppContainer = createAppContainer(DrawerAppNavigator);
+
+export default AppContainer;
